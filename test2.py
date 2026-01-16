94a11741860e7bfdeafc01c5e96c6d24c7befacb
ubuntu <aitechmanml@gmail.com>
2026-01-16 22:39:51 +0530

    minor changes

--- test2.py
+++
@@ -1,9 +0,0 @@
-import gymnasium
-import PyFlyt.gym_envs
-
-env = gymnasium.make("PyFlyt/QuadX-Pole-Balance-v4", render_mode="human")
-
-term, trunc = False, False
-obs, _ = env.reset()
-while not (term or trunc):
-    obs, rew, term, trunc, _ = env.step(env.action_space.sample())
